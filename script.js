83a10b23279919cf757eb9b6cade5a027ac9ee8a
6danilo1c <danilo.bernardina@escola.pr.gov.br>
2025-10-07 11:30:57 -0300

    Danilo

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const avança = document.querySelectorAll('.btn-proximo');
 avança.forEach(button => {
     button.addEventeLidtener('click', function(){
         const atual = document.querySelector('.ativo');
-        const proximopasso = 'passo-' + this.getAttribute('data-proximo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
         atual.classList.remove('ativo');
-        document.getElementById(proximoPasso).classList.add
+        document.getElementById(proximoPasso).classList.add('ativo');
     })
 })
